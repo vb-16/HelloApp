d5de35e1e6690d2899cec711ab1eee98fc208f41
Vaishnavi <vaishnavibharat.170807@gmail.com>
2026-03-16 20:49:52 +0530

    Coded UC3: Display Hello with Command-Line Argument or Default Message

--- HelloApp.java
+++ HelloApp.java
@@ -2,13 +2,9 @@ public class HelloApp {
 
     public static void main(String[] args) {
 
-        if (args.length > 0) {
-            String name = args[0];
-            System.out.println("Hello, " + name + "!");
-        } else {
-            System.out.println("Hello, World!");
-        }
+        String name = (args.length > 0) ? args[0] : "Vaish";
 
+        System.out.println("Hello, " + name + "!");
     }
 
 }
