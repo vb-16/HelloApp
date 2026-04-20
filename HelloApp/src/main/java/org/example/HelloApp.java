47beecbabcd03f2af704dfe03cd84a2aab7dd4c6
Vaishnavi <vaishnavibharat.170807@gmail.com>
2026-04-20 08:29:39 +0530

    Coded UC3: Default name when no argument provided

---
+++ HelloApp/src/main/java/org/example/HelloApp.java
@@ -0,0 +1,8 @@
+package HelloApp.src.main.java.org.example;
+
+public class HelloApp {
+    public static void main(String[] args) {
+        String name = (args.length > 0) ? args[0] : "World";
+        System.out.println("Hello, " + name + "!");
+    }
+}
